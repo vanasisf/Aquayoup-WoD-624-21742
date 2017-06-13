c0d905f51d7a652acda9d87d25cdc7be464ca6cb
Stitch <stitchounet@orange.fr>
2017-06-14 00:17:41 +0200

    Retire les portails customs spawn
    Retire les portails customs (PNJ & GOB) spawn dans la DB comme les portails pour Baie du Butin , Cabestan , Valgarde , Vashj ir  , Donjon de la bravoure , etc

---
+++ DBs/Custom/2017_06_13_03_world_Retirer les portails customs capitales.sql
@@ -0,0 +1,64 @@
+DELETE FROM `world`.`creature` WHERE `creature`.`id` = 1100000;
+DELETE FROM `world`.`creature` WHERE `creature`.`id` = 1100001;
+DELETE FROM `world`.`creature` WHERE `creature`.`id` = 1100002;
+DELETE FROM `world`.`creature` WHERE `creature`.`id` = 1100003;
+DELETE FROM `world`.`creature` WHERE `creature`.`id` = 1100004;
+DELETE FROM `world`.`creature` WHERE `creature`.`id` = 1100005;
+DELETE FROM `world`.`creature` WHERE `creature`.`id` = 1100013;
+DELETE FROM `world`.`creature` WHERE `creature`.`id` = 1100015;
+DELETE FROM `world`.`creature` WHERE `creature`.`id` = 1100016;
+DELETE FROM `world`.`creature` WHERE `creature`.`id` = 1100017;
+DELETE FROM `world`.`creature` WHERE `creature`.`id` = 1100018;
+DELETE FROM `world`.`creature` WHERE `creature`.`id` = 1100019;
+DELETE FROM `world`.`creature` WHERE `creature`.`id` = 1100020;
+DELETE FROM `world`.`creature` WHERE `creature`.`id` = 1100021;
+DELETE FROM `world`.`creature` WHERE `creature`.`id` = 1100022;
+DELETE FROM `world`.`creature` WHERE `creature`.`id` = 1100023;
+DELETE FROM `world`.`creature` WHERE `creature`.`id` = 1100025;
+DELETE FROM `world`.`creature` WHERE `creature`.`id` = 1100026;
+DELETE FROM `world`.`creature` WHERE `creature`.`id` = 1100027;
+DELETE FROM `world`.`creature` WHERE `creature`.`id` = 1100028;
+DELETE FROM `world`.`creature` WHERE `creature`.`id` = 1100029;
+DELETE FROM `world`.`creature` WHERE `creature`.`id` = 1100030;
+DELETE FROM `world`.`creature` WHERE `creature`.`id` = 1100031;
+DELETE FROM `world`.`creature` WHERE `creature`.`id` = 1100032;
+DELETE FROM `world`.`creature` WHERE `creature`.`id` = 1100033;
+DELETE FROM `world`.`creature` WHERE `creature`.`id` = 1100034;
+DELETE FROM `world`.`creature` WHERE `creature`.`id` = 1100035;
+DELETE FROM `world`.`creature` WHERE `creature`.`id` = 1100036;
+DELETE FROM `world`.`creature` WHERE `creature`.`id` = 1100037;
+DELETE FROM `world`.`creature` WHERE `creature`.`id` = 1100038;
+DELETE FROM `world`.`creature` WHERE `creature`.`id` = 1100039;
+DELETE FROM `world`.`creature` WHERE `creature`.`id` = 1100040;
+DELETE FROM `world`.`creature` WHERE `creature`.`id` = 1100050;
+DELETE FROM `world`.`creature` WHERE `creature`.`id` = 1100051;
+DELETE FROM `world`.`creature` WHERE `creature`.`id` = 1100056;
+
+
+DELETE FROM `world`.`gameobject` WHERE `gameobject`.`guid` = 612076;
+DELETE FROM `world`.`gameobject` WHERE `gameobject`.`guid` = 601517;
+DELETE FROM `world`.`gameobject` WHERE `gameobject`.`guid` = 601516;
+DELETE FROM `world`.`gameobject` WHERE `gameobject`.`guid` = 601515;
+DELETE FROM `world`.`gameobject` WHERE `gameobject`.`guid` = 612079;
+DELETE FROM `world`.`gameobject` WHERE `gameobject`.`guid` = 612078;
+DELETE FROM `world`.`gameobject` WHERE `gameobject`.`guid` = 612077;
+DELETE FROM `world`.`gameobject` WHERE `gameobject`.`guid` = 612073;
+DELETE FROM `world`.`gameobject` WHERE `gameobject`.`guid` = 612074;
+DELETE FROM `world`.`gameobject` WHERE `gameobject`.`guid` = 612075;
+DELETE FROM `world`.`gameobject` WHERE `gameobject`.`guid` = 612080;
+DELETE FROM `world`.`gameobject` WHERE `gameobject`.`guid` = 612081;
+DELETE FROM `world`.`gameobject` WHERE `gameobject`.`guid` = 612082;
+DELETE FROM `world`.`gameobject` WHERE `gameobject`.`guid` = 612083;
+DELETE FROM `world`.`gameobject` WHERE `gameobject`.`guid` = 201797;
+DELETE FROM `world`.`gameobject` WHERE `gameobject`.`guid` = 601549;
+DELETE FROM `world`.`gameobject` WHERE `gameobject`.`guid` = 601550;
+DELETE FROM `world`.`gameobject` WHERE `gameobject`.`guid` = 601552;
+DELETE FROM `world`.`gameobject` WHERE `gameobject`.`guid` = 612087;
+DELETE FROM `world`.`gameobject` WHERE `gameobject`.`guid` = 612086;
+DELETE FROM `world`.`gameobject` WHERE `gameobject`.`guid` = 612085;
+DELETE FROM `world`.`gameobject` WHERE `gameobject`.`guid` = 601552;
+DELETE FROM `world`.`gameobject` WHERE `gameobject`.`guid` = 601552;
+DELETE FROM `world`.`gameobject` WHERE `gameobject`.`guid` = 601552;
+
+
+
