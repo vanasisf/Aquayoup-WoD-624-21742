cdc1c35293e484ca40a0e13a9cbd8a931729282e
Stitch <stitchounet@orange.fr>
2017-07-11 03:50:21 +0200

    Suppression Generic Quest Invisibility
    Aura qui masquaient des pnjs jusqu'a une certaine condition ce qui rendait le jeu tres lineaire ou pouvait bloquer le joueur si une des conditions etait ignoré ou bugué . La contre partie de cette suppression etant de croiser parfois le meme pnj mais avec une idee differente .

---
+++ DBs/world/2017-07-11_00 Suppression Generic Quest Invisibility.sql
@@ -0,0 +1,106 @@
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 49414; # Generic Quest Invisibility 1
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 163465; # Generic Quest Invisibility 1
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 84877; # Generic Quest Invisibility 1 (Vehicle) 
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 49415; # Generic Quest Invisibility 2
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 84908; # Generic Quest Invisibility 2 (Vehicle)
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 60921; # Generic Quest Invisibility 3
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 85076; # Generic Quest Invisibility 3  (Vehicle)
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 82343; # Generic Quest Invisibility 4
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 85314; # Generic Quest Invisibility 4  (Vehicle)
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 80797; # Generic Quest Invisibility 5
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 85399; # Generic Quest Invisibility 5  (Vehicle)
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 82358; # Generic Quest Invisibility 6
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 85809; # Generic Quest Invisibility 6  (Vehicle)
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 85096; # Generic Quest Invisibility 7
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 92034; # Generic Quest Invisibility 7  (Vehicle)
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 78718; # Generic Quest Invisibility 8
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 84886; # Generic Quest Invisibility 9
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 80852; # Generic Quest Invisibility 10
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 83305; # Generic Quest Invisibility 11
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 83323; # Generic Quest Invisibility 12
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 84020; # Generic Quest Invisibility 13
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 86877; # Generic Quest Invisibility 14
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 81717; # Generic Quest Invisibility 14
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 83080; # Generic Quest Invisibility 15
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 84667; # Generic Quest Invisibility 16
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 90442; # Generic Quest Invisibility 17
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 87237; # Generic Quest Invisibility 18
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 84957; # Generic Quest Invisibility 19
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 85813; # Generic Quest Invisibility 20
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 87871; # Generic Quest Invisibility 21
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 87872; # Generic Quest Invisibility 22
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 89954; # Generic Quest Invisibility 23
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 94224; # Generic Quest Invisibility 24
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 94223; # Generic Quest Invisibility 25
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 94222; # Generic Quest Invisibility 26
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 101058; # Generic Quest Invisibility 27 (Vehicle)
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 89302; # Generic Quest Invisibility 27
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 89303; # Generic Quest Invisibility 28
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 89304; # Generic Quest Invisibility 29
+UPDATE `creature_template_addon` SET `auras` = NULL WHERE `creature_template_addon`.`auras` = 93696; # Generic Quest Invisibility 29 (Vehicle)
+
+UPDATE `world`.`creature_template_addon` SET `auras` = '46598' WHERE `creature_template_addon`.`entry` = 45400;
+REPLACE INTO `creature_template_addon` (`entry`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES
+(41154, 0, 0, 65536, 1, 0, '76991'),
+(41256, 0, 0, 65536, 1, 0, '29266'),
+(43983, 0, 0, 65536, 1, 0, '29266'),
+(43985, 0, 0, 65536, 1, 0, '29266'),
+(47606, 0, 0, 65536, 1, 0, '76651 88703'),
+(52886, 0, 0, 65536, 1, 0, '97767');
+UPDATE `world`.`creature_template_addon` SET `auras` = '97768' WHERE `creature_template_addon`.`entry` = 52887;
+UPDATE `world`.`creature_template_addon` SET `auras` = '97770' WHERE `creature_template_addon`.`entry` = 52889;
+REPLACE INTO `creature_template_addon` (`entry`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES
+(48563, 0, 0, 65536, 1, 0, '88639'),
+(48598, 0, 0, 65536, 1, 0, '29266'),
+(53887, 0, 0, 65536, 1, 0, '100337');
+REPLACE INTO `creature_template_addon` (`entry`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES
+(44540, 0, 0, 65536, 1, 0, '76040'),
+(52890, 0, 0, 65536, 1, 0, '97772'),
+(52936, 0, 0, 65536, 1, 0, '96733'),
+(53736, 0, 0, 65536, 1, 0, '99927');
+UPDATE `world`.`creature_template_addon` SET `auras` = '99628' WHERE `creature_template_addon`.`entry` = 53649;
+REPLACE INTO `creature_template_addon` (`entry`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES
+(42389, 0, 0, 65536, 1, 0, '74096 75602'),
+(44631, 0, 0, 3, 1, 0, '78148 78150'),
+(44644, 0, 0, 65539, 1, 0, '78148 78150'),
+(45034, 0, 0, 0, 1, 0, '78148 78150'),
+(49414, 0, 0, 65536, 1, 0, '88435'),
+(49415, 0, 0, 65536, 1, 0, '88435'),
+(52309, 0, 0, 65536, 1, 0, '84531');
+UPDATE `world`.`creature_template_addon` SET `auras` = '95843' WHERE `creature_template_addon`.`entry` = 49284;
+REPLACE INTO `creature_template_addon` (`entry`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES
+(47940, 0, 0, 65536, 1, 0, '89196'),
+(52948, 0, 0, 65536, 1, 0, '97921 96824'),
+(53212, 0, 0, 65536, 1, 0, '97921 96824'),
+(53213, 0, 0, 65536, 1, 0, '97921 96824'),
+(54036, 0, 0, 65536, 1, 0, '100608 96824'),
+(54037, 0, 0, 65536, 1, 0, '100610 96824'),
+(54038, 0, 0, 65536, 1, 0, '100608 96824'),
+(54039, 0, 0, 65536, 1, 0, '100610 96824'),
+(54040, 0, 0, 65536, 1, 0, '100608 96824');
+REPLACE INTO `creature_template_addon` (`entry`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES
+(45035, 0, 0, 65536, 1, 0, '78148 78150'),
+(45036, 0, 0, 65536, 1, 0, '78148 78150'),
+(45039, 0, 0, 65536, 1, 0, '78148 78150');
+REPLACE INTO `creature_template_addon` (`entry`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES
+(45510, 0, 0, 65536, 1, 0, '85630'),
+(45511, 0, 0, 65536, 1, 0, '85632'),
+(45693, 0, 0, 65536, 1, 0, '85633'),
+(45694, 0, 0, 65536, 1, 0, '85630');
+UPDATE `world`.`creature_template_addon` SET `auras` = '51329' WHERE `creature_template_addon`.`entry` = 53659;
+UPDATE `world`.`creature_template_addon` SET `auras` = '29266' WHERE `creature_template_addon`.`entry` = 51196;
+UPDATE `world`.`creature_template_addon` SET `auras` = '29266 94523' WHERE `creature_template_addon`.`entry` = 50634;
+REPLACE INTO `creature_template_addon` (`entry`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES
+(50672, 0, 0, 65536, 1, 214, '89695'),
+(53373, 0, 0, 65536, 1, 0, '98891 99037');
+UPDATE `world`.`creature_template_addon` SET `auras` = '100480' WHERE `creature_template_addon`.`entry` = 53905;
+UPDATE `world`.`creature_template_addon` SET `auras` = '63048' WHERE `creature_template_addon`.`entry` = 48027;
+
+
+
+
+
+
+
+
+
